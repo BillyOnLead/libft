fb3a9e94f679be87741c429a6dde547814e23ccd
Anas Zari <azari@e2r10p12.1337.ma>
2022-10-17 15:14:31 +0100

    4th_push

--- ft_split.c
+++ ft_split.c
@@ -6,18 +6,18 @@
 /*   By: azari <azari@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 09:39:34 by azari             #+#    #+#             */
-/*   Updated: 2022/10/17 10:24:58 by azari            ###   ########.fr       */
+/*   Updated: 2022/10/17 14:50:34 by azari            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	count(char const *s, int c)
+int	count(char *s, int c)
 {
 	int i;
 	int count;
 	
-	i = 0;
+	i = -1;
 	count = 0;
 	while (s[++i])
 	{
@@ -27,11 +27,27 @@ int	count(char const *s, int c)
 	return (count);
 }
 
+void free_str(char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+		free(str[i]);
+	free(str);
+}
+
 char **ft_split(const char *s, char c)
 {
 	int		i;
 	int		j;
 	char	**ptr;
 
-	
+	ptr = (char **)malloc(sizeof(char *) * (count(str, c) + 1));
+}
+
+int main()
+{
+	char str[] = "///x/xxxx/xxx/xx//x/////xx//x///";
+	printf("%d", count(str, '/'));
 }
